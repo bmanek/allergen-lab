22b7af40f42e8036debd1666f18d7dcfbdaeec74
mmorales4050 <mmorales4050@gmail.com>
2019-03-21 14:33:54 -0400

    created controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'ingredients/index'
+
+  get 'ingredients/show'
+
+  get 'recipes/index'
+
+  get 'recipes/show'
+
+  get 'users/show'
 end
